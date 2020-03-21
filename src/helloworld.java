6ec55219b01d1fd205b23106e7c0629064419c8d
USER <USER@DESKTOP-5RJEP71>
2020-03-21 21:01:00 +0900

    Create a menu

---
+++ src/helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("*** Select menu ***");
+		System.out.println(" 1. Add thing");
+		System.out.println(" 2. Delete thing");
+		System.out.println(" 3. Edit thing");
+		System.out.println(" 4. View thing");
+		System.out.println(" 5. Exit");
+	}
+
+}
